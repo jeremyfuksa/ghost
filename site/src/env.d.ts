010fd28e17b1835aa9c302a2a21ae5db2e977084
Jeremy Fuksa <hello@orangefla.me>
2026-04-25 18:37:09 -0500

    feat(site): add typed Ghost Content API client with contract tests

--- site/src/env.d.ts
+++ site/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly GHOST_URL: string;
+  readonly GHOST_CONTENT_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
